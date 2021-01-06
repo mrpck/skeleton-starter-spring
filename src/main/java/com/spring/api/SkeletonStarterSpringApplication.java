4c944df3d780b654791282d10be143c8e3a782b5
Michele Rosica <michelerosica@gmail.com>
2021-01-06 01:14:48 +0100

    fix component

--- src/main/java/com/spring/api/SkeletonStarterSpringApplication.java
+++ src/main/java/com/spring/api/SkeletonStarterSpringApplication.java
@@ -2,12 +2,15 @@ package com.spring.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.spring.controller.MainController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = MainController.class)
 public class SkeletonStarterSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkeletonStarterSpringApplication.class, args);
 	}
-
 }
